f134c8e8a3c4988a7e81b6a6c841ca292196db23
Evandro Dogakiuchi <85240626+evandrodogakiuchi@users.noreply.github.com>
2022-07-06 14:16:04 -0300

    feat: added hooks

---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+
+const Home = props => (
+    <div className="Home">
+        <PageTitle
+            title="React Hooks"
+            subtitle="Version 16.8" />
+    </div>
+)
+
+export default Home
