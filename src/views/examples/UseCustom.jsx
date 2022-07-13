4531de87b4b8f4750144aa82dfab4cd0f950bf93
Evandro Dogakiuchi <85240626+evandrodogakiuchi@users.noreply.github.com>
2022-07-13 15:02:56 -0300

    feat: added UseCustom Hook

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useCounter } from '../../hooks/useCounter'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseRef = (props) => {
+
+    const [count, inc, dec] = useCounter()
+
     return (
         <div className="UseCustom">
             <PageTitle
-                title="Seu Hook"
-                subtitle="Vamos aprender como criar o nosso próprio Hook!"
+                title="Your Hook"
+                subtitle="How to create our own Hook!"
             />
+            <SectionTitle title="Exercise #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn" onClick={() => dec()}>-1</button>
+                    <button className="btn" onClick={() => inc()}>+1</button>
+                </div>
+            </div>
         </div>
     )
 }
